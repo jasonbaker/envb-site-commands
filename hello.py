728cebad9346faf9aed836dbe898ff8f17c09b7d
Jason Baker <amnorvend@gmail.com>
2010-04-06 12:09:42 -0500

    Adding a few extra example commands.

--- hello.py
+++ hello.py
@@ -16,3 +16,31 @@ class hellosh(Command):
     name = 'hello'
     def run(self, args, config):
         sh("echo 'Hello, world!'")
+
+class helloweb(Command):
+    """
+    Run a webserver that prints hello world
+    """
+    name='helloweb'
+    py_dependencies=['cherrypy']
+    def run(self, args, config):
+        import cherrypy
+        class HelloWorld(object):
+            def index(self):
+                return 'Hello, world!'
+            index.exposed=True
+        cherrypy.quickstart(HelloWorld())
+
+class named_hello(Command):
+    """
+    Prints hello to a specified user.
+    """
+    name = 'named_hello'
+    def get_arg_parser(self):
+        parser = self.get_base_arg_parser()
+        parser.add_argument('--name', default='world',
+                            help='Specify who we are greeting')
+        return parser
+
+    def run(self, args, config):
+        notify('Hello, %s!' % args.name)
